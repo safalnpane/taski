b169faa2097d75f045aa2896c842567db8f18bcd
Safal Neupane <safal.npane@gmail.com>
2023-08-13 22:38:50 +1000

    chore(backend): setup express

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+// app.js
+
+const express = require("express");
+
+const app = express();
+const port = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Taski server running on port ${port}`);
+});

ade12fb87efef17f0e09359be19737f6ba5b4f68
Safal Neupane <safal.npane@gmail.com>
2023-08-20 16:41:05 +1000

    feat(backend): add api for check and star endpoint

---
+++ backend/api/check_todo.sh
@@ -0,0 +1,6 @@
+# Toggle 'check'
+
+curl -s -X PUT "http://localhost:3001/check/64e1b28e2c2482dfa3b49f0d" \
+  -H 'Content-Type: application/json'
+
+
---
+++ backend/api/star_todo.sh
@@ -0,0 +1,6 @@
+# Toggle 'star'
+
+curl -s -X PUT "http://localhost:3001/star/64e1b28e2c2482dfa3b49f0d" \
+  -H 'Content-Type: application/json'
+
+
